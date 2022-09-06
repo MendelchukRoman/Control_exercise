c0a59c5804af46961fd5e5c0905ffb714971f1e5
kadzuki <heaven.roma@mail.ru>
2022-09-07 00:49:43 +0300

    Создание второго массива

--- DZ_control/Program.cs
+++ DZ_control/Program.cs
@@ -2,4 +2,5 @@
             Console.WriteLine("Первоначальный массив");
             string first_massiv = Console.ReadLine();
             Massiv = first_massiv.Split(' ');
-          
+            var result = new string[Massiv .Length];
+            var realSize = 0;
