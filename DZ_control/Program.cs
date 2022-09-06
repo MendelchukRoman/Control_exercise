640f91bd4f878dfd641984729aa0ee7854c8396c
kadzuki <heaven.roma@mail.ru>
2022-09-07 00:47:07 +0300

    Ввод изначального массива

---
+++ DZ_control/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] Massiv;
+            Console.WriteLine("Первоначальный массив");
+            string first_massiv = Console.ReadLine();
+            Massiv = first_massiv.Split(' ');
+          
